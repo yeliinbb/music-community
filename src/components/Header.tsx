28b1522b985ff8ea11b8df52a7cf8336e594be8f
hyeonw8 <whgusrud223@gmail.com>
2024-07-09 20:42:06 +0900

    fix : Header img 추가 및 CSS 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,10 +2,25 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header>
-      <p>CyTunes</p>
+    <header className="flex items-center justify-between p-5 px-[65px]">
+      <p className="text-2xl font-bold">CyTunes</p>
       <div>
-        <input type="text" placeholder="검색어를 입력해 주세요" autoFocus />
+        <input
+          type="text"
+          placeholder="검색어를 입력해 주세요"
+          autoFocus
+          className="w-72 p-2 border border-gray-300 rounded"
+        />
+      </div>
+      <div className="flex flex-row items-center">
+        <div className="mr-5">
+          <button>logout</button>
+        </div>
+        <img
+          src="https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg"
+          alt="profile image"
+          className="w-10 h-10 object-cover rounded-full"
+        />
       </div>
     </header>
   );
