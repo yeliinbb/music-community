19d6adfa9b3c57a9a03aabf0619d0f0f2b16e78b
youngsupark1 <eddy364885@gmail.com>
2024-07-09 12:37:32 +0900

    ✨ update : 타입 추가

--- src/types/auth.type.ts
+++ src/types/auth.type.ts
@@ -3,3 +3,18 @@ export type FormState = {
   password: string;
   nickname: string;
 };
+
+// 회원 정보
+export type User = {
+  id: string;
+  email: string;
+  nickname: string;
+  createdAt: string;
+};
+
+// 인증 상태와 관련된 정보
+export type AuthState = {
+  user: User | null;
+  loading: boolean;
+  error: string | null;
+};
