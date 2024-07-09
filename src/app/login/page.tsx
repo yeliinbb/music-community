23bf4fad68b420ae4ee924edb5386b1ecaa48857
youngsupark1 <eddy364885@gmail.com>
2024-07-09 11:06:37 +0900

    ✨ update : UI 작업 중

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,7 +3,51 @@ import React from "react";
 const loginPage = () => {
   return (
     <div>
-      <h1>Welcome back!</h1>
+      <div className="flex justify-center items-center min-h-screen">
+        <div className="w-full max-w-lg bg-gray-800 p-8 shadow-md rounded-lg">
+          <div className="text-center mb-8">
+            <h2 className="mt-4 text-2xl font-bold text-gray-200">Welcome back!</h2>
+          </div>
+          <form>
+            <div className="mb-6">
+              <label className="block text-gray-400 text-sm font-bold mb-2" htmlFor="email">
+                Email
+              </label>
+              <input
+                type="email"
+                className="w-full px-3 py-2 bg-gray-800 border-b-2 border-blue-400 text-gray-200 focus:outline-none focus:border-blue-400"
+              />
+            </div>
+            <div className="mb-6">
+              <label className="block text-gray-400 text-sm font-bold mb-2" htmlFor="password">
+                PassWord
+              </label>
+              <input
+                type="password"
+                className="w-full px-3 py-2 bg-gray-800 border-b-2 border-blue-400 text-gray-200 focus:outline-none focus:border-blue-400"
+              />
+            </div>
+            <div className="flex justify-between items-center">
+              <div>
+                <button
+                  type="submit"
+                  className="ml-20 px-10 py-3 border border-blue-400 text-blue-400 font-bold hover:bg-blue-400 hover:text-gray-800 transition-colors "
+                >
+                  Login
+                </button>
+              </div>
+              <div>
+                <button
+                  type="submit"
+                  className="mr-20 px-10 py-3 border border-blue-400 text-blue-400 font-bold hover:bg-blue-400 hover:text-gray-800 transition-colors "
+                >
+                  SignUp
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
