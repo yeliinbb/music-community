b65209d3d53d88a0762482dcab6d6a5c1800958b
yeliinbb <156063434+yeliinbb@users.noreply.github.com>
2024-07-09 22:07:41 +0900

    add : 헤더 컴포넌트 추가

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,9 +1,11 @@
 import React, { PropsWithChildren } from "react";
 import NavigationBar from "./_components/NavigationBar";
+import Header from "@/components/Header";
 
 const MainLayout = ({ children }: PropsWithChildren) => {
   return (
     <main className="w-full h-full bg-[#D9D9D9] rounded-3xl relative">
+      <Header />
       <NavigationBar />
       {children}
     </main>
