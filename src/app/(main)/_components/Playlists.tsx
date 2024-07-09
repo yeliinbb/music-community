1b7c074e5a08115fc977c41671cca3b9f66970d4
yeliinbb <156063434+yeliinbb@users.noreply.github.com>
2024-07-10 00:09:52 +0900

    fix : 라우터 로직 수정 및 타입 추가

--- src/app/(main)/_components/Playlists.tsx
+++ src/app/(main)/_components/Playlists.tsx
@@ -6,13 +6,8 @@ import { useParams, useSearchParams } from "next/navigation";
 import React, { use } from "react";
 
 const Playlists = () => {
-  //   const searchParams = useSearchParams();
-  //   const playlistId = searchParams.get("id");
-  //   const params = useParams();
-  //   console.log("params", params);
-  //   console.log("playlistId", playlistId);
   const { data, isPending, isError, isSuccess } = useQuery({
-    queryKey: ["trending"],
+    queryKey: ["playlists"],
     queryFn: async () => {
       const response = await axios(`/api/spotify/playlists`);
       console.log("response => ", response);
