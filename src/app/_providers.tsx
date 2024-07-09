9b6ee110257cb096fa08095806f0a6d31af8d530
yeliinbb <156063434+yeliinbb@users.noreply.github.com>
2024-07-09 10:26:05 +0900

    스포티파이 데이터 보내는 로직 구현

--- src/app/_providers.tsx
+++ src/app/_providers.tsx
@@ -1,6 +1,7 @@
-import React, { PropsWithChildren, useState } from 'react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+"use client";
+import React, { PropsWithChildren, useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const Providers = ({ children }: PropsWithChildren) => {
   const [queryClient] = useState(
