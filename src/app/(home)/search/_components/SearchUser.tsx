10631cf6ec25dfc2c079ea15e84be90778af3182
butterbeetle <butterbeetle@naver.com>
2024-07-09 15:39:22 +0900

    Style: 유저도 앨범,아티스트랑 비슷하게 꾸밈

--- src/app/(home)/search/_components/SearchUser.tsx
+++ src/app/(home)/search/_components/SearchUser.tsx
@@ -2,6 +2,7 @@
 
 import api from "@/api/api";
 import { useQuery } from "@tanstack/react-query";
+import Image from "next/image";
 import { useSearchParams } from "next/navigation";
 
 export default function SearchUser() {
@@ -15,5 +16,25 @@ export default function SearchUser() {
   });
 
   console.log("USER DATA___", searchUsers);
-  return <div>{searchUsers?.map((searchUser) => <li key={searchUser.id}>{searchUser.nickname}</li>)}</div>;
+  return (
+    <div>
+      <h2 className="font-bold text-xl">유저</h2>
+      <ul className="grid grid-cols-6 gap-4">
+        {searchUsers?.map((searchUser) => (
+          <li key={searchUser.id}>
+            <div className="relative aspect-square p-2">
+              <Image
+                src={"http://via.placeholder.com/640x480"}
+                className="object-cover"
+                fill
+                alt={searchUser.nickname!}
+                sizes={"100px"}
+              />
+            </div>
+            {searchUser.nickname}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
