a289a1c7f50334d7633d9945fd937a1e558f7c6e
youngsupark1 <eddy364885@gmail.com>
2024-07-09 20:03:22 +0900

    ✨ update : 회원가입, 로그인, 로그아웃 라우터 핸들러 생성 및 server.ts 쿠키 관련 로직 주석처리

---
+++ src/app/api/signUp/route.ts
@@ -0,0 +1,22 @@
+import { FormState } from "@/types/auth.type";
+import { createClient } from "@/utils/supabase/server";
+
+export async function POST(request: Request) {
+  const supabase = createClient();
+  const { email, password, nickname } = (await request.json()) as FormState;
+
+  let { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      data: {
+        nickname
+      }
+    }
+  });
+  if (error) {
+    console.log("error message:", error.message);
+  }
+
+  return Response.json({ errorMsg: error?.message || null });
+}
