1cfea80de5397107324859cc2d442c242a698c4e
hyeonw8 <whgusrud223@gmail.com>
2024-07-09 20:08:25 +0900

    fix : Route Handlers props 수정

--- src/app/api/spotify/track/[id]/route.ts
+++ src/app/api/spotify/track/[id]/route.ts
@@ -3,18 +3,20 @@ import { SpotifyTrack } from "@/types/spotify.type";
 import axios from "axios";
 import { NextRequest, NextResponse } from "next/server";
 
-interface GetProps {
-  request: NextRequest;
-  params: { id: string };
-}
-
-export const GET = async ({ request, params }: GetProps) => {
+export const GET = async(request: NextRequest, { params }: { params: { id: string } }) => {
   try {
     const token = await getAccessToken();
+    // console.log('--------------------------------')
+    // console.log('Access Token:', token);
+    // // console.log('Params:', params.id)
+    console.log("Track ID:", params.id);
     const response = await axios.get<SpotifyTrack>(`https://api.spotify.com/v1/tracks/${params.id}`, {
-      headers: { Authorization: `Bearer ${token}` },
-      params: { market: "KR" }
+      headers: { Authorization: `Bearer ${token}` }
+      // params: { market: "KR" }
     });
+    console.log("--------------------------------");
+    console.log("--------------------------------");
+    console.log(response.data);
     return NextResponse.json(response.data);
   } catch (error) {
     console.error("Error fetching track:", error);
