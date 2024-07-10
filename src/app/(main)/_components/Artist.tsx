936a48dd6a964410a4977731befc8b405a7e0b37
hyeonw8 <whgusrud223@gmail.com>
2024-07-10 20:55:32 +0900

    update : 캐러셀 기능 추가 및 artistId 데이터 가져오기 로직 추가
    로직 수정 반드시 필요..!

--- src/app/(main)/_components/Artist.tsx
+++ src/app/(main)/_components/Artist.tsx
@@ -1,99 +1,126 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
-import Slider from "react-slick";
+import React from "react";
+import axios from "axios";
+
+import Slider, { CustomArrowProps } from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 import type { SpotifyArtist } from "@/types/spotify.type";
+import { useQuery } from "@tanstack/react-query";
+import { MainArtist } from "@/types/main_artist";
 
 const Artist = () => {
-  const artistId = [
-    "0LigV2SSsRZaeX2htEFXV9",
-    "4Va11kshAHkYONJgZqhi0C",
-    "2c3IakpImjWyeXNvyyGsdn",
-    "7AVa6rcpTQWVqgy91llPP5",
-    "6WeDO4GynFmK4OxwkBzMW8",
-    '7jFUYMpMUBDL4JQtMZ5ilc',
-    '3zYyfrb4r6ZHI5Di0rB9bV'
-  ];
-  const [artistData, setArtistData] = useState<SpotifyArtist[] | null>([]);
-  const [error, setError] = useState<string | null>(null);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const {
+    data: artistIds,
+    isLoading: isArtistLoading,
+    isError: isArtistError,
+    error: artistError
+  } = useQuery<(string | null)[], Error>({
+    queryKey: ["artistIds"],
+    queryFn: async () => {
+      const { data } = await axios.get<MainArtist[]>("/api/artist");
+      return data.map((artist) => artist.artistId);
+    }
+  });
 
-  useEffect(() => {
-    const fetchArtistData = async () => {
-      //console.log(params)
-      try {
-        const responses = await Promise.all(
-          artistId.map(async (artistId) => {
-            const response = await fetch(`/api/spotify/artist/${artistId}`);
-            if (!response.ok) {
-              throw new Error("서버 응답이 올바르지 않습니다.");
-            }
-            return response.json();
-          })
-        );
-        setArtistData(responses);
-      } catch (err) {
-        console.error("Error fetching track data:", err);
-        setError("가수 정보를 불러오는데 실패했습니다.");
-      } finally {
-        setIsLoading(false);
-      }
-    };
+  const {
+    data: artistData,
+    isLoading: isArtistDataLoading,
+    isError: isArtistDataError,
+    error: artistDataError
+  } = useQuery<SpotifyArtist[], Error>({
+    queryKey: ["artistData", artistIds],
+    queryFn: async (): Promise<SpotifyArtist[]> => {
+      if (!artistIds) return []; // artistIds가 없으면 빈 배열 반환
+      const responses = await Promise.all(
+        artistIds.map(async (artistId) => {
+          const response = await fetch(`/api/spotify/artist/${artistId}`);
+          if (!response.ok) {
+            throw new Error("서버 응답이 올바르지 않습니다.");
+          }
+          return response.json();
+        })
+      );
+      return responses;
+    },
+    enabled: !!artistIds // artistIds가 존재할 때만 쿼리 실행
+  });
 
-    fetchArtistData();
-  }, []);
+  const CustomPrevArrow = (props: CustomArrowProps) => (
+    <div {...props} className="custom-arrow custom-prev-arrow">
+      {/* {'<'} */}
+      <img src="arrow-left.png" alt="이전 아티스트" />
+    </div>
+  );
 
-  if (isLoading) {
+  const CustomNextArrow = (props: CustomArrowProps) => (
+    <div {...props} className="custom-arrow custom-next-arrow">
+      {/* {">"} */}
+      <img src="arrow-right.png" alt="다음 아티스트" />
+    </div>
+  );
+
+  if (isArtistLoading || isArtistDataLoading) {
     return <div>--- 데이터 수집 중 ---</div>;
   }
 
-  if (error) {
-    console.error(error);
-    return <div className="text-xl">에러가 발생했습니다: {error}</div>;
+  if (isArtistError) {
+    console.error(artistError);
+    return <div className="text-xl">에러가 발생했습니다: {artistError.message}</div>;
   }
 
-  if (!artistData) {
+  if (isArtistDataError) {
+    console.error(artistDataError);
+    return <div className="text-xl">에러가 발생했습니다: {artistDataError.message}</div>;
+  }
+
+  if (!artistData || artistData.length === 0) {
     return <div className="text-xl">트랙 데이터가 없습니다.</div>;
   }
 
-  console.log(artistData);
-
   const settings = {
-    dots: true,
+    // dots: true,
     infinite: true,
     slidesToShow: 4,
-    slidesToScroll: 1,
-    arrows: true
+    slidesToScroll: 3,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 10000,
+    prevArrow: <CustomPrevArrow />,
+    nextArrow: <CustomNextArrow />
   };
 
   return (
-    <div className="w-[800px]">
+    <div className="w-[700px]">
       <p className="font-bold mb-5">아티스트</p>
-      <Slider {...settings}>
+      <div className="custom-slider ml-5">
+        <Slider {...settings}>
           {artistData.map((artist) => (
-            <div key={artist.id} className="flex flex-col">
-              <div className='flex justify-center'>
-              {artist.images ? (
-                <img src={artist.images[2].url} alt={`${artist.name}`} className="w-20 h-20 " />
-              ) : (
-                <img
-                  src="https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg"
-                  alt="artist image"
-                  className="w-10 h-10 object-cover rounded-md"
-                />
-              )}
+            <div key={artist.id} className="flex flex-col items-center">
+              <div className="flex justify-center">
+                {artist.images ? (
+                  <img
+                    src={artist.images[2].url}
+                    alt={`${artist.name}`}
+                    className="w-20 h-20 rounded-md object-cover"
+                  />
+                ) : (
+                  <img
+                    src="https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg"
+                    alt="artist image"
+                    className="w-20 h-20 object-cover rounded-full"
+                  />
+                )}
               </div>
-              
-              
-              <p className="truncate text-center" title={artist.name}>
+              <p className="truncate text-center mt-2 w-full" title={artist.name}>
                 {artist.name}
               </p>
             </div>
           ))}
-      </Slider>
+        </Slider>
+      </div>
     </div>
   );
 };
