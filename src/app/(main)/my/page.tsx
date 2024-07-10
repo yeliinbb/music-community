7e73d68a03e9752c09667c4a3e0170e9256c9823
butterbeetle <butterbeetle@naver.com>
2024-07-10 10:44:35 +0900

    Refactor: Suspense 삭제

--- src/app/(main)/my/page.tsx
+++ src/app/(main)/my/page.tsx
@@ -1,7 +1,6 @@
 import api from "@/api/api";
 import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
 import { Metadata } from "next";
-import { Suspense } from "react";
 import MyLikes from "./_components/MyLikes";
 import MyPosts from "./_components/MyPosts";
 
@@ -22,14 +21,10 @@ export default async function MyPage() {
     <div>
       <h1 className="text-center text-2xl font-bold">My Page</h1>
       <HydrationBoundary state={dehydrate(queryClient)}>
-        <Suspense fallback={<div>내 포스트 로우딩...</div>}>
-          <h2 className="font-bold text-2xl">내 게시글</h2>
-          <MyPosts />
-        </Suspense>
-        <Suspense fallback={<div>좋아요 아티스트 로우딩...</div>}>
-          <h2 className="font-bold text-2xl">좋아요 한 아티스트</h2>
-          <MyLikes />
-        </Suspense>
+        <h2 className="font-bold text-2xl">내 게시글</h2>
+        <MyPosts />
+        <h2 className="font-bold text-2xl">좋아요 한 아티스트</h2>
+        <MyLikes />
       </HydrationBoundary>
     </div>
   );
