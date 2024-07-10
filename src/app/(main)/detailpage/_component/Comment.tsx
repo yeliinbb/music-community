893a329b8dc38f179c821f95b3ced5ddde00ccb5
parksumi <parksumi0127@gmail.com>
2024-07-10 21:39:58 +0900

    update: 댓글 추가 기능 구현

--- src/app/(main)/detailpage/_component/Comment.tsx
+++ src/app/(main)/detailpage/_component/Comment.tsx
@@ -1,47 +1,61 @@
 "use client";
-import { CommentType } from "@/types/comments";
 import { createClient } from "@/utils/supabase/client";
-import { useQuery } from "@tanstack/react-query";
-import React, { useState } from "react";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import React, { useRef } from "react";
 
 const Comment = ({ id }: { id: string }) => {
-  const [commentList, setCommentList] = useState<CommentType[]>([]);
+  const commentRef = useRef<HTMLInputElement>(null);
+  const queryClient = useQueryClient();
 
-  const { isError } = useQuery({
+  const { data: commentList, isError } = useQuery({
     queryKey: ["comments"],
     queryFn: async () => {
-      const supabase = createClient();
-      const { data } = await supabase.from("comments").select("id, createdAt,users(nickname, email), content");
-      console.log(data);
-      setCommentList(data);
+      try {
+        const supabase = createClient();
+        const { data } = await supabase.from("comments").select("*,users(nickname, email)");
+        console.log(data);
+        return data;
+      } catch (error) {
+        console.log(error);
+      }
     }
   });
 
-  // useEffect(() => {
-  //   const getComment = async () => {
-  //     const supabase = createClient();
-  //     const { data } = await supabase.from("comments").select("id, createdAt,users(nickname, email), content");
-  //     // .eq("postId", id);
-  //     console.log(data);
-  //     setCommentList(data);
-  //   };
-  //   getComment();
-  // }, []);
+  const { mutateAsync: createComment } = useMutation({
+    mutationFn: (newComment: any) => addComment(newComment),
+    onSuccess: () => {}
+  });
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const comment = commentRef.current?.value;
+    const newComment = { content: comment, postId: id, userId: "37a68d71-1c40-4a40-b21f-cb49d28ce82c" }; // userId 바꾸기
+    console.log(newComment);
+    const response = await createComment(newComment);
+  };
+
+  const addComment = async (newComment: any) => {
+    const supabase = createClient();
+    const response = await supabase.from("comments").insert(newComment);
+  };
 
   return (
     <div className="w-full border-2 border-gray-300 h-[400px] rounded-lg p-5">
       <h3 className="text-xl mb-2">Comment</h3>
       <div className="flex flex-col justify-center items-center">
-        <form></form>
-        <input
-          type="text"
-          className="w-[88%] outline-none indent-2.5 h-16 rounded-lg border-2 border-gray-300 bg-inherit mb-5"
-        />
+        <form className="relative w-[88%]" onSubmit={(e) => handleSubmit(e)}>
+          <input
+            ref={commentRef}
+            type="text"
+            className="w-full outline-none indent-2.5 h-16 rounded-lg border-2 border-gray-300 bg-inherit mb-5 "
+          />
+          <button className=" absolute top-[20px] right-[25px]">추가</button>
+        </form>
 
         <div className="w-[90%] h-[230px] overflow-auto">
-          {commentList.map((comment) => (
+          {commentList?.map((comment) => (
             <div className="shadow p-4 rounded-lg mb-2" key={comment.id}>
-              <p>{comment.users.nickname}</p>
+              <p>{comment.users?.nickname}</p>
               <p>{comment.content}</p>
             </div>
           ))}
