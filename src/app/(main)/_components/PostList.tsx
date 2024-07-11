526c82d4c2055a355fb5e9b18c5c27006bc4f727
hyeonw8 <whgusrud223@gmail.com>
2024-07-12 04:09:18 +0900

    add : PostList 컴포넌트 생성

---
+++ src/app/(main)/_components/PostList.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PostList = () => {
+  return (
+    <div className="mt-10">
+        <p className="font-bold mb-5">게시글</p>
+    </div>
+  )
+}
+
+export default PostList
