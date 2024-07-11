1d600c638197a8aabd6c8666901c96586c85e2ac
hyeonw8 <whgusrud223@gmail.com>
2024-07-11 20:47:56 +0900

    update :  유저 정보 새로고침이 사라지는 부분 수정 - persist 이용

--- src/store/auth.ts
+++ src/store/auth.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 type LoginState = {
   isLogin: boolean;
@@ -8,10 +9,25 @@ type LoginState = {
   setUserId: (userId: string) => void;
 };
 
-export const useLoginStore = create<LoginState>((set) => ({
-  isLogin: false,
-  login: () => set({ isLogin: true }),
-  logout: () => set({ isLogin: false }),
-  userId: "",
-  setUserId: (userId) => set({ userId })
-}));
+export const useLoginStore = create(
+  persist<LoginState>(
+    (set) => ({
+      isLogin: false,
+      userId: "",
+      login: () => set({ isLogin: true }),
+      logout: () => set({ isLogin: false }),
+      setUserId: (userId) => set({ userId })
+    }),
+    {
+      name: "userIdStorage"
+    }
+  )
+);
+
+// export const useLoginStore = create<LoginState>(persist()(set) => ({
+//   isLogin: false,
+//   login: () => set({ isLogin: true }),
+//   logout: () => set({ isLogin: false }),
+//   userId: "",
+//   setUserId: (userId) => set({ userId })
+// }));
