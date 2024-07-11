6b00d5868d254596afa3b3431adcdf6b66df39c1
youngsupark1 <eddy364885@gmail.com>
2024-07-11 22:16:27 +0900

    ✨ update : 최신 dev를 받았습니다.

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -40,51 +40,55 @@ const Login = () => {
 
   return (
     <div>
-      <div className="text-center mb-8">
-        <h2 className="mt-4 text-2xl font-bold text-black-200">Welcome back!</h2>
+      <div className="flex justify-center items-center min-h-screen">
+        <div className="w-full max-w-lg bg-[#d9d9d9] p-8 shadow-md rounded-lg">
+          <div className="text-center mb-8">
+            <h2 className="mt-4 text-2xl font-bold text-black-200">Welcome back!</h2>
+          </div>
+          <form onSubmit={handleLogin}>
+            <div className="mb-6">
+              <label className="block text-black-400 text-sm font-bold mb-2" htmlFor="email">
+                Email
+              </label>
+              <input
+                type="email"
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="w-full px-3 py-2 bg-[#d9d9d9] border-b-2 border-black focus:outline-none focus:border-[#54b2d3]"
+              />
+            </div>
+            <div className="mb-6">
+              <label className="block text-black-400 text-sm font-bold mb-2" htmlFor="password">
+                PassWord
+              </label>
+              <input
+                type="password"
+                name="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="w-full px-3 py-2 bg-[#d9d9d9] border-b-2 border-black focus:outline-none focus:border-[#54b2d3]"
+              />
+            </div>
+            <div className="flex justify-between items-center">
+              <button
+                type="submit"
+                className="ml-25 px-10 py-3 border border-black bg-[white] text-black-400 font-bold hover:bg-[#54b2d3] "
+              >
+                Login
+              </button>
+              <Link href="/signup">
+                <button
+                  type="button"
+                  className="mr-25 px-10 py-3 border border-black bg-[white] text-black-400 font-bold hover:bg-[#54b2d3] "
+                >
+                  SignUp
+                </button>
+              </Link>
+            </div>
+          </form>
+        </div>
       </div>
-      <form onSubmit={handleLogin}>
-        <div className="mb-6">
-          <label className="block text-black-400 text-sm font-bold mb-2" htmlFor="email">
-            Email
-          </label>
-          <input
-            type="email"
-            name="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="w-full px-3 py-2 bg-[#d9d9d9] border-b-2 border-black focus:outline-none focus:border-[#54b2d3]"
-          />
-        </div>
-        <div className="mb-6">
-          <label className="block text-black-400 text-sm font-bold mb-2" htmlFor="password">
-            PassWord
-          </label>
-          <input
-            type="password"
-            name="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="w-full px-3 py-2 bg-[#d9d9d9] border-b-2 border-black focus:outline-none focus:border-[#54b2d3]"
-          />
-        </div>
-        <div className="flex justify-between items-center">
-          <button
-            type="submit"
-            className="ml-20 px-10 py-3 border border-black bg-[white] text-black-400 font-bold hover:bg-[#54b2d3] "
-          >
-            Login
-          </button>
-          <Link href="/signup">
-            <button
-              type="button"
-              className="mr-20 px-10 py-3 border border-black bg-[white] text-black-400 font-bold hover:bg-[#54b2d3] "
-            >
-              SignUp
-            </button>
-          </Link>
-        </div>
-      </form>
     </div>
   );
 };
