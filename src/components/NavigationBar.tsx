fe06feef9da11679e006936fd1f2fecd239ba57e
hyeonw8 <whgusrud223@gmail.com>
2024-07-11 14:57:50 +0900

    fix : 경로 오타 수정

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -6,7 +6,7 @@ const NavigationBar = () => {
     <div className="flex flex-col gap-4 absolute right-[-85px] top-[140px] z-50">
       <NavButton href="/">Home</NavButton>
       <NavButton href="/my">My Page</NavButton>
-      <NavButton href="/detailPage">Post</NavButton>
+      <NavButton href="/detailpage">Post</NavButton>
     </div>
   );
 };
