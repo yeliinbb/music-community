e4ee133ca4a96612abadc705eafcee3837bf6229
hyeonw8 <whgusrud223@gmail.com>
2024-07-12 20:10:07 +0900

    css : 게시물 부분 여백 및 긴글 높이 수정

--- src/app/(main)/_components/PostList.tsx
+++ src/app/(main)/_components/PostList.tsx
@@ -8,7 +8,6 @@ import Link from "next/link";
 import { PostType } from "@/types/posts.type";
 import PostCard from "./PostCard";
 
-
 import { CustomNextArrow, CustomPrevArrow } from "@/components/CutomArrow";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
@@ -45,16 +44,18 @@ const PostList = () => {
   };
 
   return (
-    <div className="mt-10 w-full px-2">
+    <div className="mt-10 w-full">
       <p className="font-bold mb-5">게시글</p>
       <div className="custom-slider">
         <Slider {...settings}>
           {allPosts?.map((post) => (
             <div key={post.id} className="px-1">
-              <div className="shadow-md hover:shadow-lg transition-shadow duration-150 
-                active:shadow-[inset_0_2px_8px_gray] select-none cursor-pointer
-                border border-black rounded">
-                <Link className="p-2 flex flex-col gap-y-4" href={`/detailpage/${post.id}`}>
+              <div
+                className="shadow-md hover:shadow-lg transition-shadow duration-150 
+              active:shadow-[inset_0_2px_8px_gray] select-none cursor-pointer
+              border border-black rounded max-w-[250px] mx-auto"
+              >
+                <Link className="p-2 flex flex-col gap-y-2" href={`/detailpage/${post.id}`}>
                   <PostCard post={post} />
                 </Link>
               </div>
@@ -64,6 +65,6 @@ const PostList = () => {
       </div>
     </div>
   );
-}
+};
 
 export default PostList;
