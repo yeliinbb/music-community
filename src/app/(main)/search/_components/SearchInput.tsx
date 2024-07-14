8d6245ea8e5c11327b2a696c485d9d13870d476d
hyeonw8 <whgusrud223@gmail.com>
2024-07-14 23:35:02 +0900

    update : search 검색 시 클릭 말고 엔터로도 가능하도록 form으로 수정

--- src/app/(main)/search/_components/SearchInput.tsx
+++ src/app/(main)/search/_components/SearchInput.tsx
@@ -2,31 +2,34 @@
 
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import { useRef } from "react";
+import { FormEvent, useRef } from "react";
 
 export default function SearchInput() {
   const router = useRouter();
   const inputRef = useRef<HTMLInputElement>(null);
 
-  const onClickHandler = () => {
+  const onClickHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
     if (inputRef.current) {
       const encodeURL = encodeURIComponent(inputRef.current.value);
       router.push(`/search?params=${encodeURL}`, { scroll: false });
+      inputRef.current.value = "";
     }
   };
 
   return (
-    <div className="flex gap-x-2 w-full">
+    <form className="flex gap-x-2 w-full" onSubmit={onClickHandler}>
       <input className="px-4 py-2 w-full rounded-md" ref={inputRef} type="text" placeholder="Search..." />
       <button
         className=" select-none border border-black rounded-md p-2 bg-white font-bold
       hover:shadow-lg active:shadow-[inset_0_1px_6px_gray]"
-        onClick={onClickHandler}
+        type="submit"
+        // onClick={onClickHandler}
       >
         <div className="relative aspect-square size-[20px]">
           <Image src="/heart.svg" alt="검색 아이콘" className="object-cover" fill sizes="50px" />
         </div>
       </button>
-    </div>
+    </form>
   );
 }
