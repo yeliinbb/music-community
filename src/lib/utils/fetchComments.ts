a5d0b0f4280b9a91e7ee682bddcbe23ccc274beb
yeliinbb <156063434+yeliinbb@users.noreply.github.com>
2024-07-16 00:10:45 +0900

    fix : fetchComments.ts fetchPosts.ts 로직 변경

--- src/lib/utils/fetchComments.ts
+++ src/lib/utils/fetchComments.ts
@@ -1,5 +1,4 @@
 import { CommonCommentType } from "@/types/comment.type";
-import axios from "axios";
 
 interface FetchCommentProps {
   postId: string;
@@ -8,8 +7,20 @@ interface FetchCommentProps {
 
 export const fetchComments = async ({ postId, tableName }: FetchCommentProps): Promise<CommonCommentType[]> => {
   try {
-    const { data } = await axios.get<CommonCommentType[]>(`/api/${tableName}/${postId}`);
-    return data;
+    const response = await fetch(`/api/${tableName}/${postId}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      cache: "no-store" // 이 옵션은 필요에 따라 조정할 수 있습니다
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data as CommonCommentType[];
   } catch (error) {
     console.error("댓글 불러오기 실패", error);
     return [];
