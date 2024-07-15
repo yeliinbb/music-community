3d27b6e24b0e22f7a8e73a47636fafb5a2d3a834
hyeonw8 <whgusrud223@gmail.com>
2024-07-15 09:10:47 +0900

    update : 게시글 사진 수정 기능 추가

--- src/app/(main)/post/(postDetail)/_component/Post.tsx
+++ src/app/(main)/post/(postDetail)/_component/Post.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { deletePost, editPost } from "@/lib/utils/postUtils";
+import { deletePost, editPost, uploadImg } from "@/lib/utils/postUtils";
 import { PostType } from "@/types/posts.type";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
@@ -10,6 +10,8 @@ import { useLoginStore } from "@/store/auth";
 import PostSkeleton from "./PostSkeleton";
 import { toast } from "react-toastify";
 
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
+
 const Post = ({ id }: { id: string }) => {
   const [isEditing, setIsEditing] = useState(false);
   const titleRef = useRef<HTMLInputElement | null>(null);
@@ -17,6 +19,7 @@ const Post = ({ id }: { id: string }) => {
   const userId = useLoginStore((state) => state.userId);
   const queryClient = useQueryClient();
   const router = useRouter();
+  const [file, setFile] = useState<File | null>(null);
 
   const {
     data: post,
@@ -52,23 +55,48 @@ const Post = ({ id }: { id: string }) => {
     if (userId !== post?.userId) {
       toast.warn("작성자만 게시글을 수정할 수 있습니다");
       return;
-    } else {
+    }
+
+    if (!isEditing) {
       setIsEditing(true);
+      return;
+    }
+
+    let uploadImageURL = post?.imageURL;
+
+    try {
+      if (file) {
+        const formData = new FormData();
+        formData.append("postImg", file);
+
+        const response = await uploadImg(formData);
+        if (!response) {
+          throw new Error("Failed to upload profile picture");
+        }
+
+        uploadImageURL = `${SUPABASE_URL}/storage/v1/object/public/postsImage/${response.path}`;
+      }
+
       if (titleRef.current && contentRef.current) {
-        const title: PostType["title"] = titleRef.current?.value;
-        const content: PostType["content"] = contentRef.current?.value;
-        if (title !== undefined && content !== undefined) {
+        const title: PostType["title"] = titleRef.current.value;
+        const content: PostType["content"] = contentRef.current.value;
+
+        if ((title !== undefined && content !== undefined) || uploadImageURL !== undefined) {
           const editedPost: PostType = {
             id: id,
             title: title,
             content: content,
             created_at: post?.created_at ?? "",
-            imageURL: post?.imageURL ?? "",
+            imageURL: uploadImageURL,
             userId: userId || null
           };
+
           editMutation.mutate(editedPost);
         }
       }
+    } catch (error) {
+      console.error("Error in edit process:", error);
+      toast.error("게시글 수정 중 오류가 발생했습니다.");
     }
   };
 
@@ -92,6 +120,13 @@ const Post = ({ id }: { id: string }) => {
     }
   };
 
+  const handleFileInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = event.target.files?.[0];
+    if (selectedFile) {
+      setFile(selectedFile);
+    }
+  };
+
   if (isPending) {
     return <PostSkeleton />;
   }
@@ -104,22 +139,38 @@ const Post = ({ id }: { id: string }) => {
       {isEditing ? (
         <>
           <div className="absolute top-[10px] right-[10px] flex gap-2 text-white">
-            <button className="border border-gray-400 bg-[#CFCFCF] rounded-lg p-[3px] text-[15px]" onClick={onEdit}>
+            <button className="border border-gray-400 bg-[#54b2d3] rounded-lg p-[3px] text-[15px]" onClick={onEdit}>
               완료
             </button>
             <button className="border border-black rounded-lg bg-[#2c2c2c] p-[3px] text-[15px]" onClick={onDelete}>
               삭제
             </button>
+            <button className="border border-gray-400 rounded-lg bg-[#CFCFCF] p-[3px] text-[15px]" onClick={() => {setIsEditing(false)}}>
+              취소
+            </button>
           </div>
-
-          <div className="flex items-center h-full w-full max-h-[100px]">
-            <img src={post?.imageURL} alt="썸네일 이미지" width={100} height={100} className="rounded-md" />
+          <div className="flex flex-row mb-3 gap-x-5">
+            <div>
+              {file ? (
+                <img src={URL.createObjectURL(file)} alt="미리보기" className="rounded-md w-[100px] h-[100px]" />
+              ) : (
+                <img src={post?.imageURL} alt="현재 이미지" className="rounded-md w-[100px] h-[100px]" />
+              )}
+            </div>
             <input
-              className="text-lg mb-3 ml-8 h-8 border border-gray-500 outline-none p-1 rounded-md"
-              ref={titleRef}
-              defaultValue={post?.title ?? undefined}
-            ></input>
+              onChange={handleFileInputChange}
+              type="file"
+              id="hiddenFileInput"
+              className="mt-5"
+              accept="image/*"
+            />
           </div>
+          <input
+            className="text-lg mb-1 h-8 border border-gray-500 outline-none p-1 rounded-md"
+            ref={titleRef}
+            defaultValue={post?.title ?? undefined}
+          ></input>
+
           <textarea
             ref={contentRef}
             defaultValue={post?.content ?? undefined}
