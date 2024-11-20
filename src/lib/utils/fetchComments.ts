97da959986f520bc078bf52a76ec03fe5c8eb59d
yeliinbb <156063434+yeliinbb@users.noreply.github.com>
2024-11-20 17:30:05 +0900

    [fix] 코드주석 정리

--- src/lib/utils/fetchComments.ts
+++ src/lib/utils/fetchComments.ts
@@ -8,26 +8,6 @@ interface FetchCommentProps {
   tableName: TableName;
 }
 
-// export const fetchComments = async ({ postId, tableName }: FetchCommentProps): Promise<CommonCommentType[]> => {
-//   const supabase = createClient();
-//   try {
-//     const { data, error } = await supabase
-//       .from(tableName)
-//       .select("*,users(nickname, email)")
-//       .eq("postId", postId)
-//       .order("createdAt", { ascending: false });
-
-//     if (!data || error) {
-//       return [];
-//     }
-
-//     return data as CommonCommentType[];
-//   } catch (error) {
-//     console.error("댓글 불러오기 실패", error);
-//     return [];
-//   }
-// };
-
 export const fetchComments = async ({ postId, tableName }: FetchCommentProps): Promise<CommonCommentType[]> => {
   try {
     const apiUrl = getApiUrl();
